5befc0303ed95c3c8c9cdf2c6c33a1e99a95bf58
AGreatDude <oanarares2004@gmail.com>
2025-04-29 21:30:52 +0300

    .

--- Services/ActivityService.cs
+++ Services/ActivityService.cs
@@ -17,13 +17,13 @@ namespace src.Services
             _activityRepository = activityRepository ?? throw new ArgumentNullException(nameof(activityRepository));
         }
 
-        public List<ActivityLog> GetActivityForUser(string userCNP)
+        public List<ActivityLog> GetActivityForUser(string userCnp)
         {
-            if (string.IsNullOrWhiteSpace(userCNP))
+            if (string.IsNullOrWhiteSpace(userCnp))
             {
                 throw new ArgumentException("user cannot be found");
             }
-            return _activityRepository.GetActivityForUser(userCNP);
+            return _activityRepository.GetActivityForUser(userCnp);
         }   
 
     }
