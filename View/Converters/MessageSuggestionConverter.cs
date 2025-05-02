94598fc741297ff405cbff84c3acdb8b46c44e10
PandorAura <aurapandor@gmail.com>
2025-05-02 19:15:00 +0300

    Delete unnecessary character

--- View/Converters/MessageSuggestionConverter.cs
+++ View/Converters/MessageSuggestionConverter.cs
@@ -25,7 +25,7 @@ namespace src.View.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
-            throw new NotImplementedException(); \
+            throw new NotImplementedException();
         }
     }
 }
